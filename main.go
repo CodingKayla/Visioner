3d38fa95e385602891306688fdff49f7d441a7f5
CodingKayla <kaylaspacequeen@gmail.com>
2019-03-04 08:49:35 -0500

    starting to add boundary drawing

--- main.go
+++ main.go
@@ -3,27 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
-	//"image"
+	"image"
+	_ "image/jpeg"
+	"os"
 
+	"github.com/fogleman/gg"
 	pb "google.golang.org/genproto/googleapis/cloud/vision/v1"
 )
 
 func main() {
 
-	image := flag.String("image", "", "Image file to get annotations for")
-	storedJSON := flag.String("json", "", "JSON file containing a image annotation response")
+	imageFileName := flag.String("imageFileName", "", "imageFileName file to get annotations for")
+	storedJSON := flag.String("json", "", "JSON file containing a imageFileName annotation response")
 	flag.Parse()
 
-	if (*image != "" && *storedJSON != "") || (*image == "" && *storedJSON == "") {
-		fmt.Println("Use either -image or -json, not both or neither")
+	if (*imageFileName != "" && *storedJSON != "") || (*imageFileName == "" && *storedJSON == "") {
+		fmt.Println("Use either -imageFileName or -json, not both or neither")
 		return
 	}
-
 	var res *pb.AnnotateImageResponse
 	var err error
 
-	if *image != "" {
-		res, err = runImage(*image)
+	if *imageFileName != "" {
+		res, err = runImage(*imageFileName)
 		if err != nil {
 			panic(err)
 		}
@@ -36,20 +38,36 @@ func main() {
 		}
 	}
 
-	/*faces := res.GetFaceAnnotations()
+	_ = res
+
+	sourceFile, err := os.Open("images/basketball.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	imageData, _, err := image.Decode(sourceFile)
+
+	dc := gg.NewContextForImage(imageData)
+	dc.DrawCircle(500, 500, 400)
+	dc.SetRGB(0, 0, 0)
+	dc.Fill()
+	dc.SavePNG("out.png")
+
+	faces := res.GetFaceAnnotations()
 
 	for _, face := range faces {
 		poly := face.GetBoundingPoly()
 		verts := poly.GetVertices()
 		for _, vert := range verts {
 			fmt.Printf("%v %v\n", vert.GetX(), vert.GetY())
-		}
-	}*/
 
-	jsonString, err := responseToJSON(res)
+		}
+	}
+
+	/*jsonString, err := responseToJSON(res)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(jsonString)
+	fmt.Println(jsonString)*/
 }
